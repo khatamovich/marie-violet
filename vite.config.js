454b488876f5958669ab602392a528597dcdcdea
Мурод Турсуналиев <khatamovich@icloud.com>
2024-05-09 04:12:12 +0500

    Mea

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Enable history API fallback
-    // This makes the server respond to any path by serving index.html
-    historyApiFallback: true,
-  },
 });
